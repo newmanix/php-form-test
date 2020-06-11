4291685a8b9a988a5eee42063435e40434189771
Bill Newman <william.newman@seattlecolleges.edu>
2020-06-11 11:01:31 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+if(){//show data
+
+}else{//show form
+  echo 
+  '
+  <form action="">
+  <p>First Name <input type="text" name="FirstName" /></p>
+  <p><input type="submit" /></p>
+  </form>
+  ';
+}
+?>
